58883a7ecf3983c139d67286e733ca09d589cf3d
kranthi650 <kattakranthikumar.marolix@gmail.com>
2020-01-28 16:51:44 +0530

    Hibernate1

---
+++ src/hibernate1/Employee.java
@@ -0,0 +1,43 @@
+package hibernate1;
+
+public class Employee {
+
+	private int id;
+
+	private int mobilenumber;
+
+	private String name, address;
+
+	public int getMobilenumber() {
+		return mobilenumber;
+	}
+
+	public void setMobilenumber(int mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
